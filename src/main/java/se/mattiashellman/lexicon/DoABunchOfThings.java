76d5212f5b4d26aa0b090b20a27c9290f3b31ce9
Mattias Hellman <mattias.hellman@gmail.com>
2025-10-06 15:14:56 +0200

    Fix time formatting

--- src/main/java/se/mattiashellman/lexicon/DoABunchOfThings.java
+++ src/main/java/se/mattiashellman/lexicon/DoABunchOfThings.java
@@ -37,7 +37,7 @@ public class DoABunchOfThings {
     public static String convertSeconds(int seconds) {
         int hours = seconds / 3600;
         int minutes = seconds % 3600 / 60;
-        return hours + " : " + minutes + " : " + seconds % 60;
+        return hours + ":" + minutes + ":" + seconds % 60;
     }
 
     public static void setGuessInput(int input) {
