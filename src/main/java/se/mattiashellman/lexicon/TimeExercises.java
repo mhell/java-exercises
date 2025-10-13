f31a26977046302bc4945d4ac5a26440283da400
Mattias Hellman <mattias.hellman@gmail.com>
2025-10-13 17:15:58 +0200

    Complete 1-16 time exercises

--- src/main/java/se/mattiashellman/lexicon/TimeExercises.java
+++ src/main/java/se/mattiashellman/lexicon/TimeExercises.java
@@ -1,6 +1,6 @@
 package se.mattiashellman.lexicon;
 
-import java.time.LocalDate;
+import java.time.*;
 import java.time.format.DateTimeFormatter;
 import java.util.Locale;
 
@@ -15,5 +15,69 @@ public class TimeExercises {
         System.out.println(formattedDate.substring(0, 1).toUpperCase() + formattedDate.substring(1));
 
         // 3
+        localDate = localDate.minusWeeks(1).minusDays(localDate.getDayOfWeek().getValue() - 1);
+        for (int i=0; i < 7; i++) {
+            System.out.println("Day " + (i+1) + ": " + localDate.plusDays(i));
+        }
+
+        // 4
+        localDate = LocalDate.parse("2025-10-13");
+        System.out.println(localDate);
+
+        // 5
+        LocalDate birthday = LocalDate.parse("1982-12-02");
+        DayOfWeek dayOfWeek = birthday.getDayOfWeek();
+        System.out.println(dayOfWeek);
+
+        // 6
+        localDate = LocalDate.now().plusYears(10).minusMonths(10);
+        System.out.println(localDate.getMonth());
+
+        // 7
+        Period period = Period.between(birthday, localDate);
+        System.out.printf("%d years, %d months, %d days%n", period.getYears(), period.getMonths(), period.getDays());
+
+        // 8
+        period = Period.of(4, 7, 29);
+        localDate = LocalDate.now().plus(period);
+        System.out.println(localDate);
+
+        // 9
+        LocalTime localTime = LocalTime.now();
+        System.out.println(localTime);
+
+        // 10
+        int nanoSecs = localTime.getNano();
+        System.out.println(nanoSecs);
+
+        // 11
+        localTime = LocalTime.parse("16:40:33");
+        System.out.println(localTime);
+
+        // 12
+        localTime = LocalTime.now();
+        String formattedTime =  localTime.format(DateTimeFormatter.ofPattern("H:m:s"));
+        System.out.println(formattedTime);
+
+        // 13
+        LocalDateTime localDateTime = LocalDateTime.parse("2018-04-05T10:00");
+        System.out.printf("%d years, %d months, %d days %d hour %d minute %n", localDateTime.getYear(),
+                localDateTime.getMonthValue(), localDateTime.getDayOfMonth(), localDateTime.getHour(), localDateTime.getMinute());
+
+        // 14
+        String formattedDateTime = localDateTime.format(DateTimeFormatter.ofPattern("EEEE d MMMM H:mm", Locale.of("sv")));
+        System.out.println(formattedDateTime);
+
+        // 15
+        localDateTime = LocalDateTime.of(LocalDate.now(), LocalTime.now());
+        System.out.printf("%d years, %d months, %d days %d hour %d minute %d second %n", localDateTime.getYear(), localDateTime.getMonthValue(),
+                localDateTime.getDayOfMonth(), localDateTime.getHour(), localDateTime.getMinute(), localDateTime.getSecond());
+
+        // 16
+        localDateTime = LocalDateTime.now();
+        localDate = localDateTime.toLocalDate();
+        localTime = localDateTime.toLocalTime();
+        System.out.printf("Date: %s, Time: %s", localDate.toString(), localTime.toString());
+
     }
 }
